868221d485e0194fba7760bc2491c50b856804cf
Jason Bohan <jbohan@osv.com>
2025-03-06 14:18:08 -0500

    [m3] of and from

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { bootstrapApplication } from "@angular/platform-browser";
+import { from, of, Subscription } from "rxjs";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   template: `
     <h1>Hello from {{ name }}!</h1>
     <a target="_blank" href="https://angular.dev/overview">
@@ -10,14 +11,38 @@ import { bootstrapApplication } from '@angular/platform-browser';
     </a>
   `,
 })
-export class App implements OnInit, OnDestroy{
-ngOnInit(): void {
-throw new Error('Method not implemented.');
-}
-ngOnDestroy(): void {
-throw new Error('Method not implemented.');
-}
-  name = 'Angular';
+export class App implements OnInit, OnDestroy {
+  name = "Angular";
+
+  sub!: Subscription;
+  subArray!: Subscription;
+  subFrom!: Subscription;
+  subThreeStrings!: Subscription;
+
+  ngOnInit(): void {
+    this.sub = of(2, 4, 6, 8).subscribe((item) =>
+      console.log("Value from of: ", item)
+    );
+    this.subArray = of([2, 4, 6, 8]).subscribe((item) =>
+      console.log("Value from of array: ", item)
+    );
+    this.subFrom = from([2, 4, 6, 8]).subscribe({
+      next: (item) => console.log("Value from from: ", item),
+      error: (err) => console.log("Error: ", err),
+      complete: () => console.log("From complete"),
+    });  
+    this.subThreeStrings = of("Hello", "World", "Angular").subscribe({
+      next: (item) => console.log("Value emitted: ", item),
+      error: (err) => console.log("Error: ", err),
+      complete: () => console.log("No more values"),
+    });
+  }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe()
+    this.subArray.unsubscribe();
+    this.subFrom.unsubscribe();
+    this.subThreeStrings.unsubscribe();
+  }
 }
 
 bootstrapApplication(App);
